5d3ed9b5fdfe5b886848b117bffddca30941797a
Paul Melekhov <gugglegum@gmail.com>
2013-12-03 23:53:07 +0600

    Improved automated tests

--- pascal/GraphTasks/tests/tests.php
+++ pascal/GraphTasks/tests/tests.php
@@ -1,18 +1,46 @@
 <?php
 
-$tests = [
-    [ '1 graph1.txt', 'test1_graph1.txt' ],
-    [ '1 graph2.txt', 'test1_graph2.txt' ],
-    [ '1 graph3.txt', 'test1_graph3.txt' ],
-    [ '2 graph1.txt', 'test2_graph1.txt' ],
-    [ '2 graph2.txt', 'test2_graph2.txt' ],
-    [ '2 graph3.txt', 'test2_graph3.txt' ],
+$testTasks = [
+    1 => [
+        [ 'graph1.txt', 'test1_graph1.txt' ],
+        [ 'graph2.txt', 'test1_graph2.txt' ],
+        [ 'graph3.txt', 'test1_graph3.txt' ],
+    ],
+    2 => [
+        [ 'graph1.txt', 'test2_graph1.txt' ],
+        [ 'graph2.txt', 'test2_graph2.txt' ],
+        [ 'graph3.txt', 'test2_graph3.txt' ],
+    ],
+    5 => [
+        [ 'graph1.txt --from-point=1', 'test5_graph1_from1.txt' ],
+        [ 'graph1.txt --from-point=2', 'test5_graph1_from2.txt' ],
+        [ 'graph1.txt --from-point=3', 'test5_graph1_from3.txt' ],
+        [ 'graph1.txt --from-point=4', 'test5_graph1_from4.txt' ],
+        [ 'graph1.txt --from-point=5', 'test5_graph1_from5.txt' ],
+        [ 'graph1.txt --from-point=6', 'test5_graph1_from6.txt' ],
+        [ 'graph1.txt --from-point=7', 'test5_graph1_from7.txt' ],
+        [ 'graph3.txt --from-point=1', 'test5_graph3_from1.txt' ],
+        [ 'graph3.txt --from-point=2', 'test5_graph3_from2.txt' ],
+        [ 'graph3.txt --from-point=3', 'test5_graph3_from3.txt' ],
+        [ 'graph3.txt --from-point=4', 'test5_graph3_from4.txt' ],
+        [ 'graph3.txt --from-point=9', 'test5_graph3_from9.txt' ],
+        [ 'graph3.txt --from-point=10', 'test5_graph3_from10.txt' ],
+    ],
+    6 => [
+        [ 'graph1.txt', 'test6_graph1.txt' ],
+        [ 'graph2.txt', 'test6_graph2.txt' ],
+        [ 'graph3.txt', 'test6_graph3.txt' ],
+    ],
 ];
 
-foreach ($tests as $test) {
-    exec('..\\GraphTasks.exe ' . $test[0] . ' > _test_.tmp');
-    check('_test_.tmp', 'res/' . $test[1]);
-    unlink('_test_.tmp');
+foreach ($testTasks as $task => $tests) {
+    echo "Task{$task}\n";
+    foreach ($tests as $test) {
+        exec('..\\GraphTasks.exe ' . $task. ' ' . $test[0] . ' > _test_.tmp');
+        check('_test_.tmp', 'res/' . $test[1]);
+        unlink('_test_.tmp');
+    }
+    echo "\n";
 }
 
 function check($file1, $file2)
